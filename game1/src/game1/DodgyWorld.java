dcd438e7cf7054b8ab1f34681acfea30f3f8d414
Harry Cho <hacho@vassar.edu>
2014-10-11 19:53:08 -0400

    Change of plans in game
    New (similar but different) game! Dodge instead of catch.

---
+++ game1/src/game1/DodgyWorld.java
@@ -0,0 +1,46 @@
+package game1;
+
+import java.awt.Color;
+import java.util.Random;
+
+import tester.*;
+
+import javalib.funworld.*;
+import javalib.colors.*;
+import javalib.worldcanvas.*;
+import javalib.worldimages.*;
+
+
+public class DodgyWorld {
+    
+    // Decided to trash snake and make a new game that is NOT snake.
+    // Going to save working on snake for a later non-class-related project.
+    // However, coding snake helped with getting used to Game Worlds so now...
+    // Going to make a new game. A much simpler game that still hits the
+    // elements needed for this assignment.
+    
+    /*
+    Calling this game "Dodgy"
+    - Two blocks will fall from the top of the screen. Simple square or rectangle
+    - Player-controlled rectangle at the bottom can move left or right
+    - You must dodge both blocks. If you catch either of them, the game ends.
+    - Score is based on how long you survive
+    - Either start the game really fast, or speed it up as it goes? Speed up as in
+      decrease the interval between each pair of blocks.
+    */
+    
+    /*
+    Classes I'll need:
+    - BlockOne
+    - BlockTwo (will operate like GoodBlock)
+    - PlayerBlock (player controlled)
+    - DodgyWorld (main)
+    - Testing class(es?)
+    */
+
+
+    public static void main(String[] args) {
+
+    }
+    
+}
