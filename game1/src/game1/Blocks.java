8ef611952b62a8958a459fb7660d1ff081ec3db5
Harry Cho <hacho@vassar.edu>
2014-10-12 15:13:40 -0400

    Working with two blocks in same class
    Uh.
    Only testing later can tell if it works.

--- game1/src/game1/Blocks.java
+++ game1/src/game1/Blocks.java
@@ -12,7 +12,8 @@ import javalib.worldimages.*;
 
 public class Blocks {
 
-    public Posn posn;
+    public Posn posn; // block one?
+    public Posn posn2; // block two?
 
     public int speed;
     public int b_width; // Board width
@@ -21,8 +22,8 @@ public class Blocks {
     public final int width = 20; // Block width
     public final int height = 20; // Block height
 
-    public IColor color1 = new Blue();
-    public IColor color2 = new Red();
+    public IColor color1 = new Blue(); //block one
+    public IColor color2 = new Red(); // block two
 
     public Random random = new Random();
 
@@ -31,10 +32,12 @@ public class Blocks {
         this.b_height = b_height;
         this.speed = speed;
         this.posn = new Posn(randomX(b_width), 0);
+        this.posn2 = new Posn(randomX(b_width), 0);
     }
 
-    private Blocks(Posn posn, int b_width, int b_height, int speed) {
+    private Blocks(Posn posn, Posn posn2, int b_width, int b_height, int speed) {
         this.posn = posn;
+        this.posn2 = posn2;
         this.b_width = b_width;
         this.b_height = b_height;
         this.speed = speed;
@@ -43,10 +46,19 @@ public class Blocks {
     private int randomX(int b_width) {
         return random.nextInt(b_width - (width * 2)) + width;
     }
+    
+    // Need to make sure posn and posn2 are not the same...
+    // Or I mean I could just have this property of the game be that
+    // they don't HAVE to be separate from each other right
+    // After all, I am in control...
 
     public Blocks fall() {
-        return new Blocks(new Posn(this.posn.x, this.posn.y + speed),
-                this.b_width, this.b_height, this.speed);
+        return new Blocks(
+                new Posn(this.posn.x, this.posn.y + speed),
+                new Posn(this.posn2.x, this.posn.y + speed),
+                this.b_width,
+                this.b_height,
+                this.speed);
     }
 
     public WorldImage drawImage() {
